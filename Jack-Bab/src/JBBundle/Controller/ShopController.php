b04e7028bda25eb7f957644ff41f56552373882e
Rémi Freret <remi.freret@hotmail.fr>
2017-10-03 10:15:42 +0200

    Ajout des Entity

--- Jack-Bab/src/JBBundle/Controller/ShopController.php
+++ Jack-Bab/src/JBBundle/Controller/ShopController.php
@@ -2,12 +2,26 @@
 
 namespace JBBundle\Controller;
 
+use JBBundle\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ShopController extends DefaultController
 {
     public function indexAction()
     {
-        return $this->render('JBBundle:Default:shop.html.twig');
+
+        $product = $this->getDoctrine()
+          ->getRepository(Product::class)
+          ->findAll();
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found'
+            );
+        }
+
+        return $this->render('JBBundle:Default:shop.html.twig', array(
+                      'produits' => $product,
+                    ));
     }
 }
