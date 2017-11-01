805b36db680c20047dd112352aa0a443a8793312
matthieujan <matthieu.jan56@gmail.com>
2017-11-01 15:06:18 +0100

    Commande form creation

--- Jack-Bab/src/JBBundle/Controller/OrderController.php
+++ Jack-Bab/src/JBBundle/Controller/OrderController.php
@@ -3,11 +3,51 @@
 namespace JBBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
-class OrderController extends DefaultController
+
+use Symfony\Component\HttpFoundation\Request;
+
+use JBBundle\Entity\Commande;
+
+class OrderController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('JBBundle:Default:order.html.twig');
+        $commande = new Commande();
+        $formBuilder = $this->get('form.factory')->createBuilder(FormType::class,$commande);
+
+        $formBuilder
+            ->add('email',EmailType::class)
+            ->add('firstName',TextType::class)
+            ->add('lastName',TextType::class)
+
+            ->add('dateRetrait',DateType::class)
+
+            ->add('cardNumber',TextType::class)
+            ->add('crypto',TextType::class)
+            ->add('dateExp',DateType::class)
+
+            ->add('Valider',SubmitType::class)
+        ;
+
+        $form = $formBuilder->getForm();
+
+
+        if($request->isMethod('POST')){
+
+            $form->handleRequest($request);
+            if($form->isValid()){
+                return $this->redirectToRoute('jb_payment',array('commande'=>$commande));
+            }
+        }
+
+        return $this->render('JBBundle:Default:order.html.twig',array(
+            'form' => $form->createView(),
+        ));
     }
 }
---
+++ Jack-Bab/src/JBBundle/Entity/Commande.php
@@ -0,0 +1,283 @@
+<?php
+
+namespace JBBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Commande
+ *
+ * @ORM\Table(name="commande")
+ * @ORM\Entity(repositoryClass="JBBundle\Repository\CommandeRepository")
+ */
+class Commande
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstName", type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lastName", type="string", length=255)
+     */
+    private $lastName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="cardNumber", type="string", length=255)
+     */
+    private $cardNumber;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="crypto", type="string", length=255)
+     */
+    private $crypto;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="dateExp", type="date")
+     */
+    private $dateExp;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="dateRetrait", type="date")
+     */
+    private $dateRetrait;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(name="listProduit", type="array")
+     */
+    private $listProduit;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Commande
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     *
+     * @return Commande
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     *
+     * @return Commande
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set cardNumber
+     *
+     * @param string $cardNumber
+     *
+     * @return Commande
+     */
+    public function setCardNumber($cardNumber)
+    {
+        $this->cardNumber = $cardNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get cardNumber
+     *
+     * @return string
+     */
+    public function getCardNumber()
+    {
+        return $this->cardNumber;
+    }
+
+    /**
+     * Set crypto
+     *
+     * @param string $crypto
+     *
+     * @return Commande
+     */
+    public function setCrypto($crypto)
+    {
+        $this->crypto = $crypto;
+
+        return $this;
+    }
+
+    /**
+     * Get crypto
+     *
+     * @return string
+     */
+    public function getCrypto()
+    {
+        return $this->crypto;
+    }
+
+    /**
+     * Set dateExp
+     *
+     * @param \DateTime $dateExp
+     *
+     * @return Commande
+     */
+    public function setDateExp($dateExp)
+    {
+        $this->dateExp = $dateExp;
+
+        return $this;
+    }
+
+    /**
+     * Get dateExp
+     *
+     * @return \DateTime
+     */
+    public function getDateExp()
+    {
+        return $this->dateExp;
+    }
+
+    /**
+     * Set dateRetrait
+     *
+     * @param \DateTime $dateRetrait
+     *
+     * @return Commande
+     */
+    public function setDateRetrait($dateRetrait)
+    {
+        $this->dateRetrait = $dateRetrait;
+
+        return $this;
+    }
+
+    /**
+     * Get dateRetrait
+     *
+     * @return \DateTime
+     */
+    public function getDateRetrait()
+    {
+        return $this->dateRetrait;
+    }
+
+    /**
+     * Set listProduit
+     *
+     * @param array $listProduit
+     *
+     * @return Commande
+     */
+    public function setListProduit($listProduit)
+    {
+        $this->listProduit = $listProduit;
+
+        return $this;
+    }
+
+    /**
+     * Get listProduit
+     *
+     * @return array
+     */
+    public function getListProduit()
+    {
+        return $this->listProduit;
+    }
+}
+
