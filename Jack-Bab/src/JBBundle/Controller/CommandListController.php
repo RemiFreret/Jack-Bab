35fbec35c3d64707ad91884152420a088adfaeb2
matthieujan <matthieu.jan56@gmail.com>
2017-11-02 16:42:45 +0100

    Ajout du listing brut des commandes

--- Jack-Bab/src/JBBundle/Controller/CommandListController.php
+++ Jack-Bab/src/JBBundle/Controller/CommandListController.php
@@ -8,6 +8,14 @@ class CommandListController extends DefaultController
 {
     public function indexAction()
     {
-        return $this->render('JBBundle:Default:commandlist.html.twig');
+        $repository = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository('JBBundle:Commande')
+        ;
+
+        $listeCommande = $repository->findAll();
+
+        return $this->render('JBBundle:Default:commandlist.html.twig',array('listeCommande' => $listeCommande));
     }
 }
